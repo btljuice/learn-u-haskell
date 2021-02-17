5507f7f1aad56df08c426c8abbc39cd2448b9d76
Alexis Trudeau <atrudeau@hopper.com>
2021-02-16 20:24:04 -0500

    Ch 10 - RPN: Add more operators

--- rpn.hs
+++ rpn.hs
@@ -1,10 +1,14 @@
 solveRPN :: String -> Double
-solveRPN expression = head $ foldl calc [] (words expression)
+solveRPN = head . foldl calc [] . words
   where calc :: [Double] -> String -> [Double]
         calc (a:b:t) "+" = (b+a):t
         calc (a:b:t) "-" = (b-a):t
         calc (a:b:t) "*" = (b*a):t
         calc (a:b:t) "/" = (b/a):t
+        calc (a:b:t) "^" = (b ** a):t
+        calc (a:t) "ln" = log a : t
+        calc t "sum" = [sum t]
         calc t n = (read n):t
 
 test1 = solveRPN "10 4 3 + 2 * -"
+test2 = solveRPN "90 34 12 33 55 66 + * - +"
